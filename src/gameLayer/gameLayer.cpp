06464be432b81245e63802c529ec3a88d3503096
shinobi018 <nithin.sai@insight-centre.org>
2024-10-05 14:03:13 +0100

    feat: add movement to player

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -11,59 +11,90 @@
 #include <gl2d/gl2d.h>
 #include <platformTools.h>
 
+struct GamePlayData
+{
+	glm::vec2 playerPos = {100, 100};
+};
 
+GamePlayData gamePlayData;
 
 gl2d::Renderer2D renderer;
 gl2d::Texture spaceshipTexture;
 
 bool initGame()
 {
-	//initializing stuff for the renderer
+	// initializing stuff for the renderer
 	gl2d::init();
 	renderer.create();
 
 	spaceshipTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true);
 
-	
-	
 	return true;
 }
-
-
 
 bool gameLogic(float deltaTime)
 {
 #pragma region init stuff
-	int w = 0; int h = 0;
-	w = platform::getFrameBufferSizeX(); //window w
-	h = platform::getFrameBufferSizeY(); //window h
-	
+	int w = 0;
+	int h = 0;
+	w = platform::getFrameBufferSizeX(); // window w
+	h = platform::getFrameBufferSizeY(); // window h
+
 	glViewport(0, 0, w, h);
-	glClear(GL_COLOR_BUFFER_BIT); //clear screen
+	glClear(GL_COLOR_BUFFER_BIT); // clear screen
 
 	renderer.updateWindowMetrics(w, h);
 #pragma endregion
 
+#pragma region movement
 
+	glm::vec2 movement = {0, 0};
 
-	renderer.renderRectangle({100,100, 100, 100}, spaceshipTexture);
+	if (
+		platform::isButtonHeld(platform::Button::W) ||
+		platform::isButtonHeld(platform::Button::Up))
+	{
+		movement.y -= 1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::A) ||
+		platform::isButtonHeld(platform::Button::Left)
+	) {
+		movement.x -= 1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::S) ||
+		platform::isButtonHeld(platform::Button::Down)
+	) {
+		movement.y += 1;
+	}
+	if (
+		platform::isButtonHeld(platform::Button::D) ||
+		platform::isButtonHeld(platform::Button::Right)
+	) {
+		movement.x += 1;
+	}
 
+	if (movement.x != 0 || movement.y != 0)
+	{
+		movement = glm::normalize(movement) * 200.f * deltaTime;
+
+		gamePlayData.playerPos += movement;
+	}
+
+#pragma region movement
+
+	renderer.renderRectangle({gamePlayData.playerPos, 100, 100}, spaceshipTexture);
 
 	renderer.flush();
 
-
-	//ImGui::ShowDemoWindow();
-
+	// ImGui::ShowDemoWindow();
 
 	return true;
 #pragma endregion
-
 }
 
-//This function might not be be called if the program is forced closed
+// This function might not be be called if the program is forced closed
 void closeGame()
 {
-
-
-
 }
