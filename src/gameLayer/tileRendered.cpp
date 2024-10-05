f5fcef16cc98127c85bbe58b73c667235ff2f773
ninjaasmoke <nithins674@gmail.com>
2024-10-05 15:18:39 +0100

    Refactor parallax rendering in TileRenderer

--- src/gameLayer/tileRendered.cpp
+++ src/gameLayer/tileRendered.cpp
@@ -4,14 +4,17 @@ void TileRenderer::render(gl2d::Renderer2D &renderer)
 {
     auto viewRect = renderer.getViewRect();
 
+    glm::vec2 parallaxDistance{viewRect.x, viewRect.y};
+    parallaxDistance *= -parallaxStrength;
+
     for (int y = -1; y <= 1; ++y)
     {
         for (int x = -1; x <= 1; ++x)
         {
-            int posX = x + int(viewRect.x / backgroundSize);
-            int posY = y + int(viewRect.y / backgroundSize);
+            int posX = x + int((viewRect.x - parallaxDistance.x) / backgroundSize);
+            int posY = y + int((viewRect.y - parallaxDistance.y) / backgroundSize);
 
-            renderer.renderRectangle(glm::vec4{glm::vec2{posX, posY}, 1, 1} * backgroundSize, texture);
+            renderer.renderRectangle(glm::vec4{glm::vec2{posX, posY}, 1, 1} * backgroundSize + glm::vec4(parallaxDistance, 0, 0), texture);
         }
     }
 }
