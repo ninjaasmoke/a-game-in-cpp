7a9bba61c0389e9e3fce8de5dd4a9f18402847a8
ninjaasmoke <nithins674@gmail.com>
2024-10-08 23:41:18 +0100

    Refactor bullet.cpp to update bullet rendering size and bullet speed

--- src/gameLayer/bullet.cpp
+++ src/gameLayer/bullet.cpp
@@ -5,11 +5,11 @@ void Bullet::render(gl2d::Renderer2D &renderer, gl2d::Texture bulletsTexture, gl
     float spaceShipAngle = atan2(fireDirection.y, -fireDirection.x);
     spaceShipAngle = glm::degrees(spaceShipAngle) + 90.f;
 
-    renderer.renderRectangle({position - glm::vec2(25,25), 50,50},
-		bulletsTexture, Colors_White, {}, spaceShipAngle, bulletAtlas.get(1, 1));
+    renderer.renderRectangle({position - glm::vec2(25,25), 40,40},
+		bulletsTexture, Colors_White, {}, spaceShipAngle, bulletAtlas.get(1, 0));
 }
 
 void Bullet::update(float deltaTime)
 {
-    position += fireDirection * deltaTime * 1500.f;
+    position += fireDirection * deltaTime * 1000.f;
 }
