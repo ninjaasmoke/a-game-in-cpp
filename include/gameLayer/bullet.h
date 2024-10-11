0411a719c554beda956a0f71f5a2aa591c653a6a
ninjaasmoke <nithins674@gmail.com>
2024-10-11 16:32:14 +0100

    Update bullet.h to set default bullet speed

--- include/gameLayer/bullet.h
+++ include/gameLayer/bullet.h
@@ -6,6 +6,8 @@ struct Bullet {
     glm::vec2 velocity = {};
     glm::vec2 fireDirection = {};
 
+    float bulletSpeed = 1000.0f;
+
     void render(gl2d::Renderer2D &renderer, gl2d::Texture bulletsTexture, gl2d::TextureAtlasPadding &bulletAtlas);
 
     void update(float deltaTime);
